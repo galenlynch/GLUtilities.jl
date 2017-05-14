8b81e792f8c1b21b25afb5b0f819c32c68905141
Galen Lynch <galen.lynch@gmail.com>
2017-05-14 13:56:09 -0400

    Add test to matlab_datevec_to_datetime

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,8 @@ using Base.Test
 
 @test time_range_to_sec(DateTime(2013, 7, 1, 0, 0, 0), 0, DateTime(2013, 7, 1, 0, 0, 1), 0) == 1
 
+@test matlab_datevec_to_datetime(Float64[2017, 05, 14, 13, 53, 22.222]) == DateTime(2017, 05, 14, 13, 53, 22, 222)
+
 @test check_overlap(1, 3, 2, 3)
 @test !check_overlap(1, 3, 4, 5)
 
