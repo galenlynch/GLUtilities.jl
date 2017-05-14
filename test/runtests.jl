fddcbe726fafa2df4110d85e9d8f448c14f42f95
Galen Lynch <galen.lynch@gmail.com>
2017-05-14 14:02:03 -0400

    Separate tests into sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,25 @@
 using GLUtilities
 using Base.Test
 
-# write your own tests here
-@test clipind(-1, 2) == 1
-@test clipind(3, 2) == 2
-@test clipind(1, 2) == 1
-@test clipind(2, 2) == 2
+@testset "GLUtilities"  begin
+    @testset "indicies" begin
+        @test clipind(-1, 2) == 1
+        @test clipind(3, 2) == 2
+        @test clipind(1, 2) == 1
+        @test clipind(2, 2) == 2
+    end
 
-@test add_time_and_micros(DateTime(2013, 7, 1), 1) == (DateTime(2013, 7, 1, 0, 0, 1), 0)
+    @testset "times" begin
+        @test add_time_and_micros(DateTime(2013, 7, 1), 1) == (DateTime(2013, 7, 1, 0, 0, 1), 0)
 
-@test time_range_to_sec(DateTime(2013, 7, 1, 0, 0, 0), 0, DateTime(2013, 7, 1, 0, 0, 1), 0) == 1
+        @test time_range_to_sec(DateTime(2013, 7, 1, 0, 0, 0), 0, DateTime(2013, 7, 1, 0, 0, 1), 0) == 1
 
-@test matlab_datevec_to_datetime(Float64[2017, 05, 14, 13, 53, 22.222]) == DateTime(2017, 05, 14, 13, 53, 22, 222)
+        @test matlab_datevec_to_datetime(Float64[2017, 05, 14, 13, 53, 22.222]) ==
+            DateTime(2017, 05, 14, 13, 53, 22, 222)
+    end
 
-@test check_overlap(1, 3, 2, 3)
-@test !check_overlap(1, 3, 4, 5)
-
+    @testset "ranges" begin
+        @test check_overlap(1, 3, 2, 3)
+        @test !check_overlap(1, 3, 4, 5)
+    end
+end
