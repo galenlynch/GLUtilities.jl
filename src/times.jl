57704009a863fa285289200b3fd5159d31c16988
Galen Lynch <galen.lynch@gmail.com>
2017-05-18 16:58:40 -0400

    Resolve type instability in matlab_datevec_to_datetime

--- src/times.jl
+++ src/times.jl
@@ -19,5 +19,5 @@ function matlab_datevec_to_datetime{T<:Real}(datevec::Array{T})
     secs = datevec[6]
     rounded_secs = floor(secs)
     millis = round(mod(1000 * secs, 1000))
-    return DateTime(datevec[1:5]..., rounded_secs, millis)
+    return DateTime(datevec[1:5]..., rounded_secs, millis)::DateTime
 end
