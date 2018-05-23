ae6e0ed9aa78c4b727c4f9dac795f1bd061a6fd5
Galen Lynch <galen.lynch@gmail.com>
2018-05-22 21:59:30 -0400

    Pre-allocations, prevent damaging types

--- src/indices.jl
+++ src/indices.jl
@@ -1,21 +1,71 @@
 "Converts an index to its time in a regularly sampled time series"
 function ndx_to_t end
-ndx_to_t(i::Real, fs::Real, start_t::Real = zero(fs)) = (i - 1) / fs + start_t
-# Can't figure out how to express either an integer or rage, so duplicating the function
-ndx_to_t(r::Range, fs::Real, start_t::Real = zero(fs)) = (r - 1) / fs + start_t
-ndx_to_t(A::AbstractArray, args...) = ndx_to_t.(A, args...)
+function ndx_to_t(
+    i::AbstractUnitRange, fs::R, start_t::R = zero(fs)
+) where {R<:Real}
+    (i - 1) / fs + start_t
+end
+function ndx_to_t(
+    i::AbstractUnitRange, fs::R, start_t::R = zero(fs)
+) where R<:Integer
+    (i - 1) / fs + start_t
+end
+function ndx_to_t(i::Real, fs::R, start_t::R = zero(fs)) where {R<:Real}
+    (i - 1) / fs + start_t
+end
+function ndx_to_t!(
+    dest::AbstractArray,
+    A::AbstractArray,
+    fs,
+    args...
+)
+    dest .= ndx_to_t.(A, fs, args...)
+end
+function ndx_to_t(
+    A::AbstractArray,
+    fs::R,
+    start_t::R = zero(fs)
+) where {R<:Integer}
+    ts = Vector{Float64}(length(A))
+    ndx_to_t!(ts, A, fs, start_t)
+end
+function ndx_to_t(
+    A::AbstractArray{R, <:Any}, fs::R, start_t::R = zero(fs)
+) where {R<:AbstractFloat}
+    ts = Vector{R}(length(A))
+    ndx_to_t!(ts, A, fs, start_t)
+end
+function ndx_to_t(
+    a::AbstractArray{T, <:Any}, fs::R, start_t::S
+) where {T, R<:Real, S<:Real}
+    P = promote_type(T,R,S)
+    promoted = convert.(P, (fs, start_t))
+    return ndx_to_t(a, promoted...)
+end
+function ndx_to_t(i::Real, fs::Real, start_t::Real)
+    return ndx_to_t(promote(i, fs, start_t)...)
+end
+
 
 "Converts a time in a regularly sampled time series to its index"
 function t_to_ndx end
-t_to_ndx(x::Real, fs::Real, start_t::Real = zero(fs)) = convert(Int, floor((x - start_t) * fs) + 1)
-t_to_ndx(A::AbstractArray, args...) = t_to_ndx.(A, args...)
+function t_to_ndx(x::Real, fs::Real, start_t::Real = zero(fs), T::DataType = Int)
+    convert(T, floor((x - start_t) * fs) + one(T))
+end
+function t_to_ndx!(dest::AbstractArray, A::AbstractArray, args...)
+    dest .= t_to_ndx.(A, args...)
+end
+function t_to_ndx(a::AbstractArray, fs, start_t = zero(fs), T::DataType = Int)
+    dest = similar(a, T)
+    t_to_ndx!(dest, a, fs, start_t)
+end
 
 "Clips an index to be within the valid range for an array of length l"
 function clip_ndx end
-clip_ndx(ind::T, l::T) where T<:Integer = min(max(ind, T(1)), l)
+clip_ndx(ind::T, l::T) where T<:Integer = min(max(ind, one(T)), l)
 clip_ndx(ind::Integer, l::Integer) = clip_ndx(promote(ind, l)...)
 
-n_ndx(start_idx::Integer, stop_idx::Integer) = stop_idx - start_idx + 1
+n_ndx(start_idx::T, stop_idx::T) where {T<:Integer} = stop_idx - start_idx + one(T)
 
 """
     ndx_offset(start_ndx, npt)
@@ -25,8 +75,8 @@ Finds the index that will select npt number of elements starting at start_ndx.
 If npt is negative, then start_ndx is treated like the end of a range of elements,
 and the index required to return npt number of elements is returned.
 """
-function ndx_offset(start_ndx::Integer, npt::Integer)
-    adjust = npt < 0 ? 1 : -1
+function ndx_offset(start_ndx::T, npt::T) where {T<:Integer}
+    adjust = npt < zero(T) ? one(T) : -one(T)
     return start_ndx + npt + adjust;
 end
 
@@ -46,9 +96,11 @@ duration(a::AbstractVector, args...) = duration(length(a), args...)
 function bin_bounds end
 # Intended to work with binno as an integer or ranges
 # though I can't figure out how to express that
-function bin_bounds(binno::Union{AbstractUnitRange{<:Integer}, Integer}, binsize::Real)
-    idx_start = (binno - 1) * binsize + 1
-    idx_stop = idx_start + binsize - 1
+function bin_bounds(binno::Union{AbstractUnitRange{T}, T}, binsize::S) where
+    {T<:Integer, S<:Integer}
+    R = promote_type(T,S)
+    idx_start = (binno - one(R)) * binsize + one(R)
+    idx_stop = idx_start + binsize - one(R)
     return (idx_start, idx_stop)
 end
 function bin_bounds(binno::Real, binsize::Real, max_ndx::Real)
@@ -59,7 +111,21 @@ end
 
 "Find the center index of a bin"
 function bin_center end
-bin_center(i::Real, args...) = mean(bin_bounds(i, args...))
+bin_center(idxs::NTuple{2, <:Real}) = mean(idxs)
+bin_center(i::Real, args...) = bin_center(bin_bounds(i, args...))
 bin_center(rs::NTuple{2, R}) where R<:Range = (rs[1] + rs[2]) / 2
 bin_center(r::Range, binsize::Real) = bin_center(bin_bounds(r, binsize))
-bin_center(a::A) where {S<:Real, T<: NTuple{2, S}, A<:AbstractArray{T}} = mean.(a)
+function bin_center!(
+    dest::AbstractArray{<:AbstractFloat, <:Any},
+    a::AbstractArray{<:NTuple{2, <:Real}}
+)
+    dest .= bin_center.(a)
+end
+function bin_center(a::AbstractArray{<:NTuple{2, F}, <:Any}) where {F<:AbstractFloat}
+    dest = similar(a, F)
+    bin_center!(dest, a)
+end
+function bin_center(a::AbstractArray{<:NTuple{2, <:Integer}, <:Any})
+    dest = similar(a, Float64)
+    bin_center!(dest, a)
+end
