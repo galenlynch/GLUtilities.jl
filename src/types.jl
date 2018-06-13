7e5d434932bfe73bab2a10d804c78c12e6d2a579
Galen Lynch <galen.lynch@gmail.com>
2018-06-13 16:35:46 -0400

    Add array input to div_type

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,7 @@
 div_type(::Type{N}) where {N<:AbstractFloat} = N
 div_type(::Type{N}) where {N<:Integer} = Float64
+div_type(::Type{A}) where {T<:AbstractFloat, N, A<:AbstractArray{T, N}} = Array{T, N}
+div_type(::Type{A}) where {T<:Integer, N, A<:AbstractArray{T, N}} = Array{Float64, N}
 function div_type(::Type{N}, ::Type{D}) where {N<:Number, D<:Number}
     div_type(promote_type(N, D))
 end
