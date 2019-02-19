bc3f9be73ecbcef560200ffaa397e92590c7c929
Galen Lynch <galen.lynch@gmail.com>
2019-02-19 00:40:08 -0500

    add full kwarg to directory reading functions

--- src/files.jl
+++ src/files.jl
@@ -1,9 +1,15 @@
-function dir_find_files(reg::Regex, dir::AbstractString = pwd())
+function dir_find_files(
+    reg::Regex, dir::AbstractString = pwd(); full::Bool = false
+)
     files = readdir(dir)
-    return filter((x) -> occursin(reg, x), files)
+    strs = full ? joinpath.(dir, files) : files
+    return filter((x) -> occursin(reg, x), strs)
 end
 
-function dir_match_files(reg::Regex, dir::AbstractString = pwd())
+function dir_match_files(
+    reg::Regex, dir::AbstractString = pwd(); full::Bool = false
+)
     files = readdir(dir)
-    return only_matches(reg, files)
+    strs = full ? joinpath.(dir, files) : files
+    return only_matches(reg, strs)
 end
